47bd7d2275c2df85a7fd7951ed274d734c7607c4
Henrik Treadup <henrik.treadup@gmail.com>
2019-01-19 13:09:06 +0100

    Bootstrap now upgrades the server and creates the ubuntu user

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,14 @@
 from fabric import task
 from fabric import SerialGroup as Group
+import time
 
 # The following tasks could be interesting.
 # 1. clean
 # 2. build
 # 3. bootstrap
 # 4. deploy
+
+REBOOT_DELAY = 17 # 17 seconds
 
 @task
 def test(ctx):
@@ -33,7 +36,25 @@ def build(ctx):
     validate_has_stage(ctx)
     ctx.run('echo Building')
 
-def bootstrap_server(c):
+def create_user(c):
+    print("Creating user")
+
+    # Add the group ubuntu if the group does not already exist
+    c.run('getent group ubuntu &>/dev/null || groupadd -r ubuntu')
+
+    # Add the user ubuntu if the user does not aleady exist
+    c.run('id -u ubuntu &>/dev/null || useradd --no-log-init -r -g ubuntu ubuntu')
+
+    # Create home folder
+    if c.run('test -d /home/ubuntu', warn=True).failed:
+        c.run('mkdir -p /home/ubuntu && chown ubuntu:ubuntu /home/ubuntu')
+
+    # Need to copy over some kind of SSH key so that I can log in as ubuntu
+    c.run('mkdir -p /home/ubuntu/.ssh/')
+    c.run('cp /root/.ssh/authorized_keys /home/ubuntu/.ssh/authorized_keys && chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys')
+
+def upgrade_server(c):
+    print("Upgrading server")
     # Run all ubuntu commands non interactively
     c.run('export DEBIAN_FRONTEND=noninteractive')
 
@@ -44,14 +65,21 @@ def bootstrap_server(c):
     # Upgrade Ubuntu
     c.run('apt-get update && apt-get --yes upgrade')
 
-    # Install Gitconfig
+    # Reboot
+    c.run('reboot')
+
+def bootstrap_server(c):
+    print("Bootstrapping server")
+    # Install Git
     c.run('apt-get --yes install git')
 
     # Install virtualenv
     c.run('apt-get --yes install virtualenv')
 
-    # Reboot
-    c.run('reboot')
+    create_user(c)
+
+def create_application(c):
+    pass
 
 @task
 def bootstrap(ctx):
@@ -64,8 +92,17 @@ def bootstrap(ctx):
 
     validate_has_stage(ctx)
 
+#    for connection in Group(*ctx.config.hosts):
+#        upgrade_server(connection)
+
+#    time.sleep(REBOOT_DELAY)
+
     for connection in Group(*ctx.config.hosts):
         bootstrap_server(connection)
+
+    for connection in Group(*ctx.config.hosts):
+        # Here I want to log in as another user.
+        create_application(connection)
 
 @task
 def deploy(ctx):
